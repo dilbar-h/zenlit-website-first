8bdebe3c8c0b26fcbf34dc9bff15b264a9911c27
Dilber Huseynzade <dilberhuseynzade@Dilbers-MacBook-Air.local>
2025-07-15 22:29:58 +0300

    Initial commit

---
+++ website/script.js
@@ -0,0 +1,13 @@
+// script.js for Zenlit under construction page
+
+document.addEventListener('DOMContentLoaded', function() {
+  setTimeout(function() {
+    document.querySelector('.phone-left')?.classList.add('animate');
+  }, 200);
+  setTimeout(function() {
+    document.querySelector('.phone-center')?.classList.add('animate');
+  }, 500);
+  setTimeout(function() {
+    document.querySelector('.phone-right')?.classList.add('animate');
+  }, 800);
+}); 
